2402e3fa570692bc5528ff07da7f051231f8d0ed
Linus Romland <romlandlinus@gmail.com>
2021-02-11 09:30:52 +0100

    moved the crawl functions to sperate file

---
+++ Crawler/Crawler.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Fizzler.Systems.HtmlAgilityPack;
+using MongoDB.Bson;
+
+namespace webcrawler
+{
+    class Crawler
+    {
+        static List<Link> links = Program.links;
+        static string startLink = "https://romland.space/";
+
+        public static void Crawl()
+        {
+            //Get initial links
+            if (links.Count == 0) GetLinksFromSite(startLink);
+
+            for (int j = 0; j < links.Count; j++)
+            {
+                Link link = links[j];
+                if (!link.visited)
+                {
+                    links[j].visited = true;
+                    MongoConnection.UpdateDB(link.link);
+
+                    GetLinksFromSite(link.link);
+                }
+            }
+        }
+
+        public static void GetLinksFromSite(string currentLink)
+        {
+            HtmlWeb html = new HtmlWeb();
+            HtmlDocument document;
+            Console.WriteLine(currentLink);
+            try
+            {
+                document = html.Load(currentLink);
+            }
+            catch
+            {
+                return;
+            }
+
+            HtmlNode page = document.DocumentNode;
+
+            List<string> linksOnSite = new List<string>();
+
+            foreach (HtmlNode item in page.QuerySelectorAll("a"))
+            {
+                if (item.GetAttributes("href").First() == null) continue;
+
+                string url = item.GetAttributes("href").First().Value;
+
+                if (!url.StartsWith("https")) continue;
+
+                //Remove URL fragment
+                int hashPos = url.IndexOf('#');
+                if (hashPos != -1)
+                {
+                    url = url.Substring(0, hashPos);
+                }
+
+                //Check if link is already on same site
+                foreach (string link in linksOnSite)
+                {
+                    if (link == url)
+                    {
+                        goto here;
+                    }
+                }
+
+                //Check if link is already in list
+                foreach (Link link in links)
+                {
+                    if (link.link == url)
+                    {
+                        goto here;
+                    }
+                }
+
+                links.Add(new Link(url, currentLink));
+                linksOnSite.Add(url);
+
+                //Insert link into MongoDB
+                BsonDocument bson = new BsonDocument { { "url", url }, { "origin", currentLink }, { "visited", false } };
+                MongoConnection.InsertToDB(bson);
+
+            here:;
+            }
+        }
+    }
+}
--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -16,92 +16,10 @@ namespace webcrawler
         {
             MongoConnection.ConnectToDB();
             links = MongoConnection.ReadAllDB();
-            Crawl();
+            Crawler.Crawl();
         }
 
+        public static List<Link> links = new List<Link>();
 
-        static string startLink = "https://romland.space/";
-
-        static List<Link> links = new List<Link>();
-
-        public static void Crawl()
-        {
-            //Get initial links
-            if (links.Count == 0) GetLinksFromSite(startLink);
-
-            for (int j = 0; j < links.Count; j++)
-            {
-                Link link = links[j];
-                if (!link.visited)
-                {
-                    links[j].visited = true;
-                    MongoConnection.UpdateDB(link.link);
-
-                    GetLinksFromSite(link.link);
-                }
-            }
-        }
-
-        public static void GetLinksFromSite(string currentLink)
-        {
-            HtmlWeb html = new HtmlWeb();
-            HtmlDocument document;
-            Console.WriteLine(currentLink);
-            try
-            {
-                document = html.Load(currentLink);
-            }
-            catch
-            {
-                return;
-            }
-
-            HtmlNode page = document.DocumentNode;
-
-            List<string> linksOnSite = new List<string>();
-
-            foreach (HtmlNode item in page.QuerySelectorAll("a"))
-            {
-                if (item.GetAttributes("href").First() == null) continue;
-
-                string url = item.GetAttributes("href").First().Value;
-
-                if (!url.StartsWith("https")) continue;
-
-                //Remove URL fragment
-                int hashPos = url.IndexOf('#');
-                if (hashPos != -1)
-                {
-                    url = url.Substring(0, hashPos);
-                }
-
-                //Check if link is already on same site
-                foreach (string link in linksOnSite)
-                {
-                    if (link == url)
-                    {
-                        goto here;
-                    }
-                }
-
-                //Check if link is already in list
-                foreach (Link link in links)
-                {
-                    if (link.link == url)
-                    {
-                        goto here;
-                    }
-                }
-
-                links.Add(new Link(url, currentLink));
-                linksOnSite.Add(url);
-
-                //Insert link into MongoDB
-                BsonDocument bson = new BsonDocument { { "url", url }, { "origin", currentLink }, { "visited", false } };
-                MongoConnection.InsertToDB(bson);
-
-            here:;
-            }
-        }
     }
 }
